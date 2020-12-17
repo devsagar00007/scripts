c5e41c885a40ba9d2b2eab2cabb01429f89d07be
devsagar00007 <75155642+devsagar00007@users.noreply.github.com>
2020-12-17 20:13:31 +0530

    Add files via upload

---
+++ player_control.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_control : MonoBehaviour
+{
+    public VariableJoystick joy;
+    float vinput;
+    float hinput;
+    [SerializeField] float speed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vinput = joy.Vertical;
+        hinput = joy.Horizontal;
+        Vector3 forwarded = transform.forward * speed;
+        Vector3 hori = transform.right * speed;
+
+        moveing(hinput, vinput, forwarded, hori);
+    }
+    void moveing(float x, float y, Vector3 forw, Vector3 hor)
+    {
+        rb.velocity = y * forw + x * hor;
+    }
+}
